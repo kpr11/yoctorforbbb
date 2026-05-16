77e882c14361484ed4a88627e3ed7a2994e16a31
Rajesh K P <rajesh.kp@hfcl.com>
2026-05-16 12:46:29 +0530

    build essential script file

---
+++ build_essentaals.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install build-essential
+
+sudo apt update
+
+sudo apt install -y \
+build-essential chrpath cpio debianutils diffstat file gawk \
+gcc git iputils-ping libacl1 locales python3 python3-git \
+python3-jinja2 python3-pexpect python3-pip python3-subunit \
+python3-yaml socat texinfo unzip wget xz-utils zstd \
+libsdl1.2-dev xterm tar
+
+sudo apt install python3-distutils
+
